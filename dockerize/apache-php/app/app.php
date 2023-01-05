373b6b760b609f823e9a91868d8fcd9b56089e22
Yves Oubida <yvesoubida@gmail.com>
2023-01-04 20:27:23 -0500

    First Load Balancer

--- dockerize/apache-php/app/app.php
+++
@@ -1,2 +0,0 @@
-<?php
-  echo 'echo from app';
--- dockerize/apache-php/app/index.php
+++
@@ -1,5 +0,0 @@
-<?php 
-    include('./app.php');
-    echo '<h1>Hello World!</h1>'.$_SERVER['SERVER_ADDR'].':'.$_SERVER['SERVER_PORT'];
-    phpinfo();
-?> 
